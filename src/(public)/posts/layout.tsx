ee4b834e493631533a2f86b8d40c3392ed371da1
Nazar-Ferents <nazarferents594@gmail.com>
2026-05-21 14:56:19 +0300

    hw1

---
+++ src/(public)/posts/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: 'PostsLayout'
+}
+type Props = { children: React.ReactNode }
+const PostsLayout = ({children}: Props) => {
+    return (
+        <>
+
+            {children}
+
+        </>
+    );
+};
+
+export default PostsLayout;
